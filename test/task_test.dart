55b112acd667d47696c2a26f917ba0eb0c88f6a6
mswift42 <martin.haesler@gmail.com>
2015-10-05 18:13:52 +0100

    Check adding contents works.

--- test/task_test.dart
+++ test/task_test.dart
@@ -9,4 +9,11 @@ void main() {
     expect(t1.scheduled, null);
     expect(t1.categories.length, 0);
   });
+  test('Adding a content inserts into contents', () {
+    var t1 = new Task('task1');
+    t1.addContent('first content');
+    t1.addContent('second content');
+    expect(t1.contents.length, 2);
+    expect(t1.contents[0], 'second content');
+  });
 }
