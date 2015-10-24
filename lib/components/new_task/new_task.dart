2d890e9c47a36c6e91180d3d900b77829f164a67
mswift42 <martin.haesler@gmail.com>
2015-10-24 20:33:44 +0100

    Add new_task component.

---
+++ lib/components/new_task/new_task.dart
@@ -0,0 +1,21 @@
+library ds.lib.components.new_task;
+
+import 'dart:html';
+import 'package:angular2/angular2.dart';
+
+@Component(
+    selector: 'new-task',
+    template: '''<div class=".form-control">
+                 <input #tsummary type="text"  class="form-control"
+                  placeholder="Add Summary"
+                  (keyup)="doneTyping(\$event)"/>
+                 </div>''')
+class NewTask {
+  doneTyping(KeyboardEvent event) {
+    if (event.keyCode == KeyCode.ENTER) {
+      InputElement e = event.target;
+      print(e.value);
+      e.value = null;
+    }
+  }
+}
