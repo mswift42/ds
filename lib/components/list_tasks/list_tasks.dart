b831a6169a9c67b7f08036fff5b300f9f4295e88
mswift42 <martin.haesler@gmail.com>
2015-11-02 16:53:04 +0000

    Display task.summary in tasklist element

--- lib/components/list_tasks/list_tasks.dart
+++ lib/components/list_tasks/list_tasks.dart
@@ -3,12 +3,14 @@ library ds.lib.components.list_tasks;
 import 'dart:html';
 import 'package:angular2/angular2.dart';
 import 'package:ds/components/list_task/list_task.dart';
+import 'package:ds/Task.dart';
 
 @Component(
     selector: 'list-tasks',
     templateUrl: 'listtasks.html',
     directives: const [ListTask, NgFor])
 
+
 class ListTasks {
-    List<String> tasklist =  ["somestuff", "morestuff"];
+    List<String> tasklist =  [new Task("task1"), new Task("task2")];
 }
--- web/main.dart
+++ web/main.dart
@@ -1,4 +1,4 @@
-import 'package:angular2/angular2.dart' show Component, bootstrap;
+import 'package:angular2/angular2.dart' ;
 import 'package:angular2/bootstrap.dart';
 import 'package:ds/components/new_task/new_task.dart';
 import 'package:ds/components/list_tasks/list_tasks.dart';
