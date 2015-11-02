dd3e6be404694dffef38b4ab07cc7a7890726cda
mswift42 <martin.haesler@gmail.com>
2015-11-02 18:23:56 +0000

    Add bootstrap css

--- web/main.dart
+++ web/main.dart
@@ -5,7 +5,7 @@ import 'package:ds/components/list_tasks/list_tasks.dart';
 
 @Component(
     selector: 'my-app',
-    template: ''''<h1>My First Angular 2 App</h1>
+    template: '''<h1>My First Angular 2 App</h1>
     <new-task></new-task><list-tasks></list-tasks>''',
     directives: const [NewTask, ListTasks])
 class AppComponent {}
