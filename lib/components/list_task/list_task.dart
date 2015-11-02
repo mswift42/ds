d0890dee48ce964ff181f277bfa85e947b92818e
mswift42 <martin.haesler@gmail.com>
2015-11-02 17:19:28 +0000

    Add listtask.html file.

--- lib/components/list_task/list_task.dart
+++ lib/components/list_task/list_task.dart
@@ -4,11 +4,8 @@ import 'package:angular2/angular2.dart';
 
 @Component(
     selector: 'list-task',
-    template:
-    '''<div class="singletask">
-        <h3>Task Summary</h3>
-        <h4>{{summary}}</h4>
-        </div>''')
+    templateUrl: 'listtask.html'
+)
 
 class ListTask {
   String summary = '';
